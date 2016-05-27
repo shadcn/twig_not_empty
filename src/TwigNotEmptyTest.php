3546f2ee722d73efd098e6b7348d16aac86bc737
Arshad Chummun <arshad@chummun.com>
2016-05-28 00:25:24 +0400

    Add support for arrays and check for access

--- src/TwigNotEmptyTest.php
+++ src/TwigNotEmptyTest.php
@@ -30,7 +30,17 @@ class TwigNotEmptyTest {
    * @return bool true if the value is not empty, false otherwise
    */
   public static function notEmpty($value) {
+    // Check for invalid array.
+    if (!is_array($value)) {
+      return false;
+    }
+
+    // If #access is provided use that.
+    if (isset($value['#access'])) {
+      return $value['#access'];
+    }
+
     // Simply check for field items for now.
-    return is_array($value) ? count(Element::children($value)) : !empty($value);
+    return count($value) ? count(Element::children($value)) : !empty($value);
   }
 }
